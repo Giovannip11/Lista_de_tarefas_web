becc6543f71d68dc21c5e9192f67e8fa59409400
Giovannip11 <giggiopinto@gmail.com>
2026-01-28 20:47:24 -0300

    Configuração do PostgreSQL no backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,11 +18,13 @@ def create_app():
     instance_path = os.path.join(basedir, "instance")
     os.makedirs(instance_path, exist_ok=True)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-        instance_path, "tarefas.db"
-    )
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    database_url = os.environ.get("DATABASE_URL")
 
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
     from .models import Tarefa  # garante que o model seja carregado
